b3ef12b0e58326d646dac905cca157144d68cd17
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-07-02 14:38:57 -0600

    Se crea la clase list

---
+++ src/Lists.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author ilope
+ */
+public interface Lists <T>  {
+    public boolean add(T t);
+    public boolean remove (T t);
+    public T find(Object id);
+    public  void showAll();
+}
